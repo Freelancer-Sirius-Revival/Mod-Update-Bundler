b1ae9142eb83b2ad600be16907fa88e5f78d81a0
Erik R <erik@erikszeug.de>
2023-01-07 22:27:22 +0100

    Add initial project files

---
+++ umainform.pas
@@ -0,0 +1,26 @@
+unit UMainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMainForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
